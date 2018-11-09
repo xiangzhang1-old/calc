119c769f881fa9e16981854e43b319901b18a6be
Xiang Zhang <zhang-xiang@hotmail.com>
2018-11-09 11:56:50 -0500

    .

--- graph.py
+++ graph.py
@@ -1,26 +1,8 @@
 # -*- coding: utf-8 -*-
 """Implements nested directed graphs. Like folders, they organize DFT computations."""
 from __future__ import print_function, division, unicode_literals
-import weakref
 import numpy as np
-
-
-def id(id):
-    """
-    The node identified by id.
-
-    (Consider) the node whose id is id.
-    Located by iterating through each segment of id.
-
-    :param Node.id id: the query
-    :return: the response to query
-    :rtype: Node
-    :raises KeyError: if the node identified by id does not exist
-    """
-    # naming shit
-    # folder.child, also KeyError
-    # where is master
-    n = node.the('master')
+import uuid, weakref
 
 
 class Node:
@@ -41,23 +23,15 @@ class Node:
         """float in (-100, 100). x coordinate when drawn in 2D by Ogma.js"""
         self.y = y if y is not None else np.random.uniform(low=-200, high=200)
         """float in (-100, 100). y coordinate"""
+        self.uuid = str(uuid.uuid1())
+        """str. UUID"""
 
-    @property
-    def id(self):
+
+class Graph(dict, Node):
+
+    def __init__(self, *args, **kwargs):
         """
-        Slash-join of all ancestors' names.
-
-        Joins the node's name, its parent's name, it's grandparent's name, all
-        the way to but excluding "root", by slash(/). It's the node's UID in
-        a nested graph.
-
-        Caveat: While a Unix filesystem look-and-feel is achieved, the root \
-        node's id is qualitatively different, which may cause problems.
-
-        :return: UID of the node, e.g. ``/PbS QD/bare qd/o(e) performance``
-        :rtype: str
+        Implements a nested directed graph.
         """
-        if parent is None:
-            return ""
-        else:
-            return parent.id + "/" + name
+        dict.__init__(self)
+        Node.__init__(self, *args, **kwargs)
