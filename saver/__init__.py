457ad8291896db1af38fd5e382bca893aef10dca
Xiang Zhang <xzhang1@mit.edu>
2019-02-25 12:49:18 -0500

    02/25 start fucking with things

--- saver/__init__.py
+++ saver/__init__.py
@@ -5,20 +5,20 @@ from .. import state
 script_dir = os.path.dirname(os.path.realpath(__file__))
 
 
-def dump(obj, file):
+def dump(obj, filename):
     r"""
     :meth:`dill.dump` `obj` to `file.dill.YYYYMMDDHHMMSS`
 
-    :param str file:
+    :param unicode filename:
     :param object obj: object to save
 
     """
-    with open("%s.dill.%s" % (file, time.strftime('%Y%m%d%H%M%S')), "wb") as f:
+    with open("%s.dill.%s" % (filename, time.strftime('%Y%m%d%H%M%S')), "wb") as f:
         dill.dump(obj, f)
 
 
-def load(file):
-    _ = sorted([_ for _ in os.listdir(script_dir) if _.startswith(file)])
+def load(filename):
+    _ = sorted([_ for _ in os.listdir(script_dir) if _.startswith(filename)])
     with open(_[-1], "rb") as f:
         return dill.load(f)
 
