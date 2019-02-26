899fa19104b4f2f734c3c6246bbd7d70fc1f1930
Xiang Zhang <xzhang1@mit.edu>
2019-02-25 21:48:32 -0500

    class Struct

--- engine/__init__.py
+++ engine/__init__.py
@@ -1,1 +1,16 @@
 """Autodiscussions eg. INCAR."""
+import pandas as pd, numpy as np
+
+class Struct(object):
+
+    def __init__(self):
+        """
+        struct in opt->struct->elecstruct
+
+        :ivar np.array(3,3) A: translation vector of crystal cell, or None
+        :ivar pd.DataFrame(x,y,z,symbol) X:
+
+        """
+        super().__init__()
+        self.A = None
+        self.X = pd.DataFrame(columns=['x', 'y', 'z', 'symbol'])
